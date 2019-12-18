718a16b25ab6e0e4d2c0105b36acddda825c6eff
angelrureLurtis <a.ruiz@lurtis.com>
2019-12-18 17:36:25 +0100

    updated for report v2

--- utilities.py
+++ utilities.py
@@ -19,9 +19,8 @@ def remove_multidim_outliers(df, columns, target, IQRm):
 
     tmp_data = df.merge(on=columns, right=outs)
 
-
-    df_no = tmp_data[~((tmp_data[target] < tmp_data[0.25] - tmp_data['IQR']*IQRm) | \
-                             (tmp_data[target] > tmp_data[0.75] + tmp_data['IQR']*IQRm))] \
+    df_no = tmp_data[(tmp_data[target] > tmp_data[0.25] - tmp_data['IQR']*IQRm) & \
+                             (tmp_data[target] < tmp_data[0.75] + tmp_data['IQR']*IQRm)] \
                           .drop(['IQR', 0.25, 0.75], axis=1)
     
     print('Removed {n_outs} points ({per_total:2.1f}%)'.format(n_outs=(df.shape[0]-df_no.shape[0]), per_total=(df.shape[0]-df_no.shape[0])*100/df.shape[0]))
